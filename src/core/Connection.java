e1776e6d025456348b1c4d274a369b4923127aa5
Guri <Guri@GurisPC>
2015-05-23 10:00:02 -0700

    Connection : updated for new each methods

--- src/core/Connection.java
+++ src/core/Connection.java
@@ -6,9 +6,40 @@ public interface Connection {
 	/**
 	 * This method returns all results form table
 	 */
-	public ResultSet getResults(String tableName);
+	public ResultSet getUsers();
+	public ResultSet getCategories();
+	public ResultSet getUsersCategories();
+	public ResultSet getPictures();
+	public ResultSet getColors();
+	public ResultSet getFonts();
+	public ResultSet getThemes();
+	public ResultSet getElements();
+	public ResultSet getTexts();
+	public ResultSet getElementsInfo();
+	public ResultSet getAlboms();
+	public ResultSet getAlbomsElements();
+	public ResultSet getAlbomsPictures();
+	public ResultSet getElementsPictures();
+	public ResultSet getAdmins();
+	
+
+	
 	/**
 	 * This method returns all results form table with specified id
 	 */
-	public ResultSet getResults(String tableName, int id);
+	public ResultSet getUsers( int id);
+	public ResultSet getCategories( int id);
+	public ResultSet getUsersCategories( int id);
+	public ResultSet getPictures( int id);
+	public ResultSet getColors( int id);
+	public ResultSet getFonts( int id);
+	public ResultSet getThemes( int id);
+	public ResultSet getElements( int id);
+	public ResultSet getTexts( int id);
+	public ResultSet getElementsInfo( int id);
+	public ResultSet getAlboms( int id);
+	public ResultSet getAlbomsElements (int id);
+	public ResultSet getAlbomsPictures( int id);
+	public ResultSet getElementsPictures( int id);
+	public ResultSet getAdmins( int id);
 }
