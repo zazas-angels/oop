4f8210fb8b0bb3ad4eda6de8900ec8698bf03431
Guri <Guri@GurisPC>
2015-05-23 14:22:13 -0700

    add getRoots

--- src/core/CategoryTreeInterface.java
+++ src/core/CategoryTreeInterface.java
@@ -8,6 +8,7 @@ import com.sun.corba.se.spi.orbutil.fsm.Guard.Result;
 public interface CategoryTreeInterface {
 	int add(CategoryInterface newOne, CategoryInterface parent);
 	ArrayList<CategoryInterface> getChilds(CategoryInterface cur);
+	ArrayList<CategoryInterface> getRoots(CategoryInterface cur);
 	CategoryInterface getParent(CategoryInterface cur);
 	int remove(CategoryInterface cur);
 }
