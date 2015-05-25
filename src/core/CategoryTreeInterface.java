3ea42ac4aa9c3430fed052353263e74c870392da
Guri <Guri@192.168.76.67>
2015-05-24 22:48:58 -0700

    added some proper methods for all childrens and parent branch

--- src/core/CategoryTreeInterface.java
+++ src/core/CategoryTreeInterface.java
@@ -9,6 +9,10 @@ import com.sun.corba.se.spi.orbutil.fsm.Guard.Result;
 public interface CategoryTreeInterface {
 	int add(CategoryInterface newOne, CategoryInterface parent);
 	List<CategoryInterface> getChilds(int id);
+	//returns all children (children of children's ....)
+	List<CategoryInterface> getChildBush(int id);
+	//returns all parents list
+	List<CategoryInterface> getParentBranch(int id);
 	List<CategoryInterface> getRoots();
 	CategoryInterface getParent(CategoryInterface cur);
 	int remove(CategoryInterface cur);
