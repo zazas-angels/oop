f4cc920edef4d306f27cb24b724a4600cd913e4c
glaba13 <glaba13@freeuni.edu.ge>
2015-05-27 04:13:17 -0700

    Just commit

--- src/core/CategoriesServlet.java
+++ src/core/CategoriesServlet.java
@@ -28,12 +28,13 @@ public class CategoriesServlet extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-
+//lalala
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+	
 	}
 
 	/**
