0df49d644576ee51d85bfd495d160cc2a3a027b0
glaba13 <glaba13@freeuni.edu.ge>
2015-05-27 13:05:55 -0700

    implemented Expand and shrink part

--- WebContent/CategoryChoosing.jsp
+++ WebContent/CategoryChoosing.jsp
@@ -1,35 +1,44 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<%-- Author guri --%>
+<%-- Author glaba13 --%>
 <%-- Java script fuctions --%>
 <%-- dummy --%>
 <script src="NextCategories.js"></script>
 <script>
-/*
- * From stack overflow
- http://stackoverflow.com/questions/2015041/two-differents-onclick-on-two-divs-one-over-the-other
- */
-function amIclicked(e, id)
-{
-    e = e || event;
-    var target = e.target || e.srcElement;
-    if(target.id==id)
-        return true;
-    else
-        return false;
-}
+	/* checking if clicked was reaaly on this div and not on super.
+	 * From stack overflow
+	 http://stackoverflow.com/questions/2015041/two-differents-onclick-on-two-divs-one-over-the-other
+	 */
+	function amIclicked(e, id) {
+		e = e || event;
+		var target = e.target || e.srcElement;
+		if (target.id == id)
+			return true;
+		else
+			return false;
+	}
 	//sending request is from W3School tutorial
 	//get expanded categories
-	function expandCategory(event,id) {
+	function expandCategory(event, id, hasChilds) {
 		alert(id);
-		if(!amIclicked(event, id))return;
-		
-		var element = document.getElementById(id+"");
+		if (hasChilds == false)
+			return;
+		if (!amIclicked(event, id))
+			return;
 
+		var element = document.getElementById(id + "");
+		//shrink this way
+		if (element.getAttribute("isExpanded")=="true") {
+			element.setAttribute("isExpanded","false");
+			shrinkCategory(event, id, hasChilds);
+			return;
+		}
+		element.setAttribute("isExpanded","true");
+		//change on click function
 		var xmlHttp;
 		if (window.XMLHttpRequest) {
 			// code for IE7+, Firefox, Chrome, Opera, Safari
@@ -42,33 +51,40 @@ function amIclicked(e, id)
 		xmlHttp.onreadystatechange = function() {
 			if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
 				element.innerHTML += xmlHttp.responseText;
-				element.innerHTML += hasChilds;
 			}
 		};
 
 		xmlHttp.open("POST", "ExpandCategoriesServlet?id=" + id, true);
 		xmlHttp.send();
-		alert(items.length);
+		//we could do so but it will goes for all sub divs and ned more complexity
+		//element.onclick=function(){shrinkCategory(event,id,hasChilds); } ;
+
 	}
-	
+	function shrinkCategory(event, id, hasChilds) {
+		alert("Axali" + id);
+		if (!amIclicked(event, id))
+			return;
+
+		var element = document.getElementById(id + "");
+		element.innerHTML = element.getAttribute("categoryName");
+	}
 </script>
 <style type="text/css">
 .not-active {
-   pointer-events: none;
-   cursor: default;
+	pointer-events: none;
+	cursor: default;
 }
 </style>
 <title>Category Chooser</title>
 </head>
 <body>
-<%-- shows all root categories --%>
-<%@ page import="core.category.CategoryTreeInterface"%>
+	<%-- shows all root categories --%>
+	<%@ page import="core.category.CategoryTreeInterface"%>
 	<%@ page import="core.category.CategoryInterface"%>
 	<%@ page import="java.util.ArrayList"%>
 	<%@ page import="java.util.List"%>
 	<%@ page import="java.io.PrintWriter"%>
 	<%
-
 		CategoryTreeInterface categories = (CategoryTreeInterface) request
 				.getServletContext().getAttribute("categories");
 		List<CategoryInterface> roots = categories.getRoots();
@@ -76,11 +92,15 @@ function amIclicked(e, id)
 		writer.print("<ul id=\"categories\" >");
 		for (int i = 0; i < roots.size(); i++) {
 			writer.print("<li>");
-			int id = roots.get(i).getId() ;
+			int id = roots.get(i).getId();
+			String categoryName = roots.get(i).getName();
 			//reaaly every root has chils , but work for every case
-			writer.print("<div id =\""+id+"\"  onclick=\"expandCategory(event,"
-					+id + ",true);\"> "
-					+ roots.get(i).getName() + " </div>");
+			writer.print("<div id =\""
+					+ id
+					+ "\"  categoryName=\""
+					+ categoryName
+					+ "\"  isExpanded = \"false\" onclick=\"expandCategory(event,"
+					+ id + ",true);\"> " + categoryName + " </div>");
 			writer.print("</li>");
 
 		}
--- src/core/ExpandCategoriesServlet.java
+++ src/core/ExpandCategoriesServlet.java
@@ -15,65 +15,78 @@ import core.category.CategoryInterface;
 import core.category.CategoryTree;
 
 /**
- * Servlet implementation class ExpandCategoriesServlet
+ * Servlet implementation class ExpandCategoriesServlet Author: glaba13
  */
 @WebServlet("/ExpandCategoriesServlet")
 public class ExpandCategoriesServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ExpandCategoriesServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	public ExpandCategoriesServlet() {
+		super();
+
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		// TODO Auto-generated method stub
-				PrintWriter writer =response.getWriter();
-				String idSting = request.getParameter("id");
-				int id;
-				try {
-					id = Integer.parseInt(idSting);
-				} catch (NumberFormatException e) {
-					id = -1;
-				}
-				if(id<0)return;
-				
-				CategoryTree categories = (CategoryTree) getServletContext().getAttribute("categories");
-				List<CategoryInterface> childCategories;
-				if(id==0){
-					childCategories=categories.getRoots();
-				}else{
-					//can be converted to 0 if roots but for every case
-					
-					childCategories=categories.getChilds(id);
-				}
-				if(childCategories==null)return;
-				writer.print("<ul>");
-				for (int i = 0; i < childCategories.size(); i++) {
-					writer.print("<li>");
-					boolean hasChilds=false;
-					int currId = childCategories.get(i).getId();
-					if(categories.hasChilds(currId))
-						hasChilds=true;
-					
-					writer.print("<div  id=\""+currId+"\" "+ "  onclick=\"expandCategory(event"+","+currId+","+hasChilds+");\""+"> "+ childCategories.get(i).getName()+" </div>");
-					writer.print("</li>");
-				}
-				writer.print("</ul>");
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	/*
+	 * makes inner categories suitable for having children
+	 * (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#doPost(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter writer = response.getWriter();
+		String idSting = request.getParameter("id");
+		int id;
+		try {
+			id = Integer.parseInt(idSting);
+		} catch (NumberFormatException e) {
+			id = -1;
+		}
+		if (id < 0)
+			return;
+
+		CategoryTree categories = (CategoryTree) getServletContext()
+				.getAttribute("categories");
+		List<CategoryInterface> childCategories;
+		if (id == 0) {
+			childCategories = categories.getRoots();
+		} else {
+			// can be converted to 0 if roots but for every case
+
+			childCategories = categories.getChilds(id);
+		}
+		if (childCategories == null)
+			return;
+		writer.print("<ul>");
+		for (int i = 0; i < childCategories.size(); i++) {
+			writer.print("<li>");
+			boolean hasChilds = false;
+			int currId = childCategories.get(i).getId();
+			if (categories.hasChilds(currId))
+				hasChilds = true;
+			String categoryName= childCategories.get(i).getName();
+			writer.print("<div  id=\"" + currId + "\" "+" categoryName=\""+categoryName
+					+ "\"  isExpanded = \"false\"  onclick=\"expandCategory(event" + "," + currId + ","
+					+ hasChilds + ");\"" + "> "
+					+ categoryName + " </div>");
+			writer.print("</li>");
+		}
+		writer.print("</ul>");
 	}
 
 }
