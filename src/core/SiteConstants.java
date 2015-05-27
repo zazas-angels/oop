93187c83ee28ad0e990e17b375c1d5a4210be0e5
glaba13 <glaba13@freeuni.edu.ge>
2015-05-27 08:08:46 -0700

    Remove for recomit

--- src/core/SiteConstants.java
+++
@@ -1,9 +0,0 @@
-package core;
-/*
- * Author guri
- */
-public final class SiteConstants {
-	public static final double USER_IMG_HEIGTH=80;
-	public static final double USER_IMG_WIDTH=80;
-	public static final String DOMAIN = "chveniSaiti.ge";
-}
