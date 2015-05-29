3d2dec5eca68e8eb621f717d84168587a6b0eff0
tripoliati <akasr13@freeuni.edu.ge>
2015-05-29 07:08:54 +0400

    hey guys, i have added an file which sends mail, its easy to understand, just need a look in file, i need some more time to finish my part, have fun. love you sooooooo much <3 one more thing, i am using for that javamail api from gmail and you need that to compile my code, you can download library from link below: https://java.net/downloads/javamail/javax.mail.jar and just add reference on that library and everything gonna be allright

---
+++ src/login_registration/sendMail.java
@@ -0,0 +1,51 @@
+package login_registration;
+
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+ 
+ 
+public class sendMail {
+	static final String SRC_MAIL_ADDRESS = "akasr13@freeuni.edu.ge"; //sends from
+	static final String SRC_MAIL_PASSWORD = "12200139";	//user password
+	
+	static Properties mailServerProperties;
+	static Session getMailSession;
+	static MimeMessage generateMailMessage;
+ 
+	public static void main(String[] args) throws AddressException, MessagingException{
+		sendEmail("akasr13@freeuni.edu.ge", "idi na xui suka" + "<br/> mase jobia");
+		sendEmail("akasr13@freeuni.edu.ge", "ara zmao, kvrias mcalia \n \n \n" + "<br/> mase jobia");
+	}
+ 
+	/*
+	 * sends message to mailAdress from SRC_MAIL_ADDRESS
+	 * message should be in HTML String format 
+	 */
+	public static void sendEmail(String mailAddress, String message) throws AddressException, MessagingException {
+
+		mailServerProperties = System.getProperties();
+		mailServerProperties.put("mail.smtp.port", "587");
+		mailServerProperties.put("mail.smtp.auth", "true");
+		mailServerProperties.put("mail.smtp.starttls.enable", "true");
+		
+		getMailSession = Session.getDefaultInstance(mailServerProperties, null);
+		generateMailMessage = new MimeMessage(getMailSession);
+		generateMailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(mailAddress));
+		generateMailMessage.setSubject("zdarovebii");
+		generateMailMessage.setContent(message, "text/html");
+		
+		Transport transport = getMailSession.getTransport("smtp");
+		
+		transport.connect("smtp.gmail.com", SRC_MAIL_ADDRESS, SRC_MAIL_PASSWORD);
+		transport.sendMessage(generateMailMessage, generateMailMessage.getAllRecipients());
+		transport.close();
+		System.out.println("message sent successfully");
+		
+	}
+}
