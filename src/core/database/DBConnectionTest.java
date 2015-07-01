93e7b4e0df50f26bd925c8bdad014c5357e8ad59
Tazo <ttora13@freeuni.edu.ge>
2015-07-01 09:13:03 +0400

    commit and push

--- src/core/database/DBConnectionTest.java
+++ src/core/database/DBConnectionTest.java
@@ -2,12 +2,58 @@ package core.database;
 
 import static org.junit.Assert.*;
 
+import java.sql.ResultSet;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.Test;
 
-public class DBConnectionTest {
+import core.StubDBConnection;
+import core.administrator.Administrator;
+import core.administrator.StubCategoryTree;
+import core.administrator.SuperAdministrator;
+import core.category.CategoryTree;
+import core.category.CategoryTreeInterface;
 
+public class DBConnectionTest {
+	/**
+	 * testing some get options
+	 */
 	@Test
-	public void test() {
+	public void testSomeGetters() {
+		Set<Integer> set = new HashSet<Integer>();
+		Connection con = new StubDBConnection(){
+			private Set<Integer> setForDB = set;
+			@Override
+			public ResultSet getUsers(int id){
+				setForDB.add(1);
+				return null;
+			}
+		};
+		con.getUsers(3);
+		assertTrue(set.contains(1));
+		con = new StubDBConnection(){
+			private Set<Integer> setForDB = set;
+			@Override
+			public ResultSet getCategories(int id){
+				setForDB.add(2);
+				return null;
+			}
+		};
+		con.getCategories(3);
+		assertTrue(set.contains(2));
+		
+		con = new StubDBConnection(){
+			private Set<Integer> setForDB = set;
+			@Override
+			public ResultSet getUsersCategories(int id){
+				setForDB.add(3);
+				return null;
+			}
+		};
+		con.getUsersCategories(3);
+		assertTrue(set.contains(3));
+		
 		
 	}
 
